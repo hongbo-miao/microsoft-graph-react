bfbe20a27c3b956fd3527d1a4d14326704d8a3f2
Hongbo Miao <Hongbo.Miao@outlook.com>
2017-08-04 13:20:49 -0700

    feat: write to excel

--- src/home/home.js
+++ src/home/home.js
@@ -6,24 +6,45 @@ class Home extends Component {
   constructor(props) {
     super(props);
 
+    const msft = hello('msft').getAuthResponse();
+
     this.state = {
-      contacts: []
+      contacts: [],
+      token: msft.access_token
     };
 
+    this.onWriteToExcel = this.onWriteToExcel.bind(this);
     this.onLogout = this.onLogout.bind(this);
   }
 
   componentDidMount() {
-    const msft = hello('msft').getAuthResponse();
+    const { token } = this.state;
 
     axios.get(
       'https://graph.microsoft.com/v1.0/me/contacts?$select=displayName,emailAddresses',
-      { headers: { Authorization: `Bearer ${msft.access_token}` }}
+      { headers: { Authorization: `Bearer ${token}` }}
     ).then(res => {
       const contacts = res.data.value;
-      console.log('res', res);
       this.setState({ contacts });
     });
+  }
+
+  onWriteToExcel() {
+    const { token, contacts } = this.state;
+
+    const values = [];
+
+    contacts.forEach(contact => {
+      values.push([contact.displayName, contact.emailAddresses[0].address]);
+    });
+
+    axios
+      .post('https://graph.microsoft.com/v1.0/me/drive/root:/demo.xlsx:/workbook/tables/Table1/rows/add',
+        { index: null, values },
+        { headers: { Authorization: `Bearer ${token}` }}
+      )
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
   }
 
   onLogout() {
@@ -61,6 +82,7 @@ class Home extends Component {
           </tbody>
         </table>
 
+        <button onClick={this.onWriteToExcel}>Write to Excel</button>
         <button onClick={this.onLogout}>Logout</button>
       </div>
     );
